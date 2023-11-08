80e19ef70b23066bd8fe21848e9a11ce9b2b8b38
Pedro López <cde.luis@gmail.com>
2023-11-08 00:45:35 -0300

    Automate The News - Exporting Data to a CSV File

---
+++ news-headlines.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import pandas as pd
+
+website = "https://www.thesun.co.uk/sport/football/"
+# path = "c:/chromedriver"
+# I put the driver on the windows path
+# https://www.selenium.dev/documentation/webdriver/troubleshooting/errors/driver_location/
+
+service = Service()
+driver = webdriver.Chrome(service=service)
+
+driver.get(website)
+
+containers = driver.find_elements(by="xpath", value='//div[@class="teaser__copy-container"]')
+
+titles = []
+subtitles = []
+links = []
+
+for container in containers:
+    title = container.find_element(by="xpath", value='./a/span').text
+    subtitle = container.find_element(by="xpath", value='./a/h3').text
+    link = container.find_element(by="xpath", value='./a').get_attribute("href")
+    titles.append(title)
+    subtitles.append(subtitle)
+    links.append(link)
+
+my_dict = {'title': titles,
+           'subtitle': subtitles,
+           'link': links}
+df_headlines = pd.DataFrame(
+    my_dict
+)
+
+df_headlines.to_csv('headlines.csv')
+
+driver.quit()
